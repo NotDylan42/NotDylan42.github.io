862dc3de43be94be54abc98fad102497150d9916
Dylan <19504203+NotDylan42@users.noreply.github.com>
2023-12-21 22:23:26 +0800

    Create comic.js

---
+++ js/comic.js
@@ -0,0 +1,38 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slider img');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function goToFirst() {
+    currentIndex = 0;
+    showSlide(currentIndex);
+}
+
+function goBack() {
+    if (currentIndex > 0) {
+        currentIndex--;
+        showSlide(currentIndex);
+    }
+}
+
+function goNext() {
+    if (currentIndex < slides.length - 1) {
+        currentIndex++;
+        showSlide(currentIndex);
+    }
+}
+
+function goToLast() {
+    currentIndex = slides.length - 1;
+    showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
